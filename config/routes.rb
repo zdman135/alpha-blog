0af51964570df7b51cbca4a3d7731cc082351c51
Rich <adbarads@gmail.com>
2022-12-30 01:26:05 -0500

    added index page and ability to get all articles and display onto frontend

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#home"
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 end
