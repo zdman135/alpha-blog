471bf4c0289636c973b565108480989b12df7af2
Rich <adbarads@gmail.com>
2022-12-31 01:56:11 -0500

    added edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#home"
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
